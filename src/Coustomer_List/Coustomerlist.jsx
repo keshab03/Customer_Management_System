1bf3dbdc5f7b6f8cc4f58a93fb3d501cfba437b5
unknown <keshabmahanta03@gmail.com>
2024-06-13 05:27:42 +0530

    pdf added

--- src/Coustomer_List/Coustomerlist.jsx
+++ src/Coustomer_List/Coustomerlist.jsx
@@ -1,6 +1,7 @@
-
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import "./coustomerlist.css";
 import customerService from '../services/customerservice';
 
@@ -15,17 +16,15 @@ const Coustomerlist = () => {
 
   const itemsPerPage = 5;
 
-
   useEffect(() => {
     const fetchdata = async () => {
-      const response = await customerService.getcustomers()
-      console.log(response.customer)
-      const data = response.customer
+      const response = await customerService.getcustomers();
+      console.log(response.customer);
+      const data = response.customer;
       setData(data);
-    }
+    };
     fetchdata();
-
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (data.length >= 0) {
@@ -38,21 +37,22 @@ const Coustomerlist = () => {
       setTotalDue(dueSum);
       setTotalExtra(extraSum);
     }
-
-  }, [data])
+  }, [data]);
 
   let deleteData = (index) => {
     customerService.deletecustomer(index);
     const newdata = data.filter(x => x._id !== index);
     setData(newdata);
-  }
-  // let slno = 0;
+  };
+
   const handleSearch = (e) => {
     setSearch(e.target.value);
   };
+
   let cancle = () => {
-    setSearch('')
-  }
+    setSearch('');
+  };
+
   let filteredData = data;
 
   if (showDue) {
@@ -97,52 +97,97 @@ const Coustomerlist = () => {
   };
 
   let sl = currentPage * 5 - 4;
+
+  const generatePDF = () => {
+    const doc = new jsPDF();
+
+    doc.text('Customer List', 20, 10);
+    doc.autoTable({
+      head: [['Sl No.', 'Name', 'Address', 'Due', 'Deposit', 'Due/Deposit']],
+      body: filteredData.map((x, index) => [
+        index + 1,
+        x.name,
+        x.address,
+        x.due,
+        x.extra,
+        x.extra - x.due > 0 ? `Deposit:${x.extra - x.due}` : `Due:${x.due - x.extra}`,
+      ]),
+      foot: [
+        ['Total', '', '', totalDue, totalExtra, totalExtra - totalDue > 0 ? `Deposit:${parseInt(totalExtra - totalDue)}` : `Due:${parseInt(totalDue - totalExtra)}`],
+      ],
+    });
+
+    doc.save('customer_list.pdf');
+  };
+
+  const generateCustomerPDF = (customer) => {
+    const doc = new jsPDF();
+
+    doc.text(`Customer: ${customer.name}`, 20, 10);
+    doc.autoTable({
+      head: [['Field', 'Value']],
+      body: [
+        ['Name', customer.name],
+        ['Address', customer.address],
+        ['Due', customer.due],
+        ['Deposit', customer.extra],
+        ['Due/Deposit', customer.extra - customer.due > 0 ? `Deposit:${customer.extra - customer.due}` : `Due:${customer.due - customer.extra}`]
+      ]
+    });
+
+    doc.save(`customer_${customer.name}.pdf`);
+  };
+
   return (
     <div>
       {data && (
         <>
-          <input id='search'
+          <input
+            id="search"
             type="text"
             placeholder="Search..."
             value={search}
             onChange={handleSearch}
-          /> <span id='span' onClick={cancle}>x</span>
-          <section id='table' key={data.id}>
+          />
+          <span id="span" onClick={cancle}>x</span>
+          <section id="table" key={data.id}>
             <table>
               <thead>
-                <th>Sl No.</th>
-                <th>Name:</th>
-                <th>Address:</th>
-                <th>Due:</th>
-                <th>Deposit:</th>
-                <th>Due/Deposit</th>
-                <th>Action</th>
+                <tr>
+                  <th>Sl No.</th>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Due</th>
+                  <th>Deposit</th>
+                  <th>Due/Deposit</th>
+                  <th>Action</th>
+                </tr>
               </thead>
               <tbody>
-                {pageData.map((x) => {
-                  // slno = slno + 1;
-                  return (
-                    <tr key={x._id}>
-                      <td>{sl++}</td>
-                      <td>{x.name}</td>
-                      <td>{x.address}</td>
-                      <td>{x.due}</td>
-                      <td>{x.extra}</td>
-                      <td>{x.extra - x.due > 0 ? (`Deposit:${x.extra - x.due}`) : (`Due:${x.due - x.extra}`)}</td>
-                      <td>
-                        <button><Link to={`/edit/${x._id}`}>Edit</Link></button>
-                        <button id='delete' onClick={() => { deleteData(x._id) }}>Delete</button>
-                      </td>
-                    </tr>
-                  );
-                })}
+                {pageData.map((x) => (
+                  <tr key={x._id}>
+                    <td>{sl++}</td>
+                    <td>{x.name}</td>
+                    <td>{x.address}</td>
+                    <td>{x.due}</td>
+                    <td>{x.extra}</td>
+                    <td>{x.extra - x.due > 0 ? `Deposit:${x.extra - x.due}` : `Due:${x.due - x.extra}`}</td>
+                    <td>
+                      <button>
+                        <Link to={`/edit/${x._id}`}>Edit</Link>
+                      </button>
+                      <button id="delete" onClick={() => deleteData(x._id)}>Delete</button>
+                      <button onClick={() => generateCustomerPDF(x)}>Download PDF</button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
               <tfoot>
                 <tr>
                   <td colSpan="3">Total:</td>
                   <td>{totalDue}</td>
                   <td>{totalExtra}</td>
-                  <td>{totalExtra - totalDue > 0 ? (`Deposit:${parseInt(totalExtra - totalDue)}`) : (`Due:${parseInt(totalDue - totalExtra)}`)}</td>
+                  <td>{totalExtra - totalDue > 0 ? `Deposit:${parseInt(totalExtra - totalDue)}` : `Due:${parseInt(totalDue - totalExtra)}`}</td>
                   <td></td>
                 </tr>
               </tfoot>
@@ -154,16 +199,22 @@ const Coustomerlist = () => {
         <button onClick={prevPage} id="prev">
           Previous
         </button>
-        <span style={{ fontSize: '20px' }}>{currentPage} of {Math.ceil(filteredData.length / itemsPerPage)}</span>
+        <span style={{ fontSize: '20px' }}>
+          {currentPage} of {Math.ceil(filteredData.length / itemsPerPage)}
+        </span>
         <button onClick={nextPage} id="next">
           Next
         </button>
       </div>
-      <div id='filterdata'>
+      <div id="filterdata">
         <button onClick={dueAmount}>Due</button>
         <button onClick={extraAmount}>Extra</button>
       </div>
+      <div id="generate-total-pdf">
+        <button onClick={generatePDF}>Download Full PDF</button>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default Coustomerlist;
--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const port=5500;
-export const url='http://localhost:5500';
+// export const url='http://localhost:5500';
+export const url='https://customer-management-system-yzki.onrender.com';
